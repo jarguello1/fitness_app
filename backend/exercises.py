c3e2e85a449870b6b94023b050edb58685d17f85
jarguello1 <jcarguello11@live.com>
2026-06-01 00:42:37 -0400

    figuring out basic functions

---
+++ backend/exercises.py
@@ -0,0 +1,12 @@
+#need to do the basic functions
+
+def find_exercise(exercise):
+    """this one has to parse an existing list of exercises"""
+
+def add_new_exercises(exercise, bodypart=None):
+    """This one adds exercise to the database"""
+
+def delete_exercises(exercise):
+    """this one parses the list to find existing exercises and deletes them"""
+
+
---
+++ backend/reps_and_sets.py
@@ -0,0 +1,8 @@
+import exercises
+
+def repetitions(exercise, reps):
+    """deals with adding and removing reps to each set"""
+    return reps
+
+def sets(exercise, reps):
+    """deals with the """
